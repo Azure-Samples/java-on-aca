3a2a6f00a0478a2beaf5a8ee82862aaacddcd29d
Songbo Wang <sonwan@microsoft.com>
2025-02-25 17:42:32 +0800

    refine the prepare steps

--- .devcontainer/funcs.sh
+++ .devcontainer/funcs.sh
@@ -2,11 +2,8 @@
 
 DEV_ENV_FILE="$HOME/.dev-environment"
 
-function saveenv() {
-    declare -p | grep "declare \(--\|-x\)" | grep -v "^declare \(--\|-x\) \(PS[0-9]\|BASH_.*\|PATH\|PWD\|LS_COLORS\)=" > "$DEV_ENV_FILE"
-}
+alias saveenv='declare -p | grep "declare \(--\|-x\)" | grep -v "^declare \(--\|-x\) \(PS[0-9]\|BASH_.*\|PATH\|PWD\|LS_COLORS\)=" > "$DEV_ENV_FILE"'
 
-function clearenv() {
-    rm -f "$DEV_ENV_FILE"
-}
+alias loadenv='source "$DEV_ENV_FILE"'
 
+alias clearenv='rm -f "$DEV_ENV_FILE"'
--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,24 +1,16 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-source "$DIR/funcs.sh"
 
 azd config set alpha.deployment.stacks on
 
-grep saveenv "$HOME/.bashrc" > /dev/null 2>&1
+grep JAVA_AI_FUNC "$HOME/.bashrc" > /dev/null 2>&1
 if [[ $? -ne 0 ]]; then
 
     cat <<EOT >> "$HOME/.bashrc"
-
-DEV_ENV_FILE="\$HOME/.dev-environment"
-
-# auto load
-if [[ -f "\$DEV_ENV_FILE" ]]; then
-    source "\$DEV_ENV_FILE"
-fi
+JAVA_AI_FUNC="$DIR"
+[ -f "\$JAVA_AI_FUNC/funcs.sh" ] && \. "\$JAVA_AI_FUNC/funcs.sh"
 
 EOT
-
-    declare -f saveenv clearenv >> "$HOME/.bashrc"
 
 fi
